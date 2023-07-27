023e8a392b4eafa780b661be97e5a545e2c4654c
Gunjoong Kim <rjsoownd0419@gmail.com>
2023-07-27 11:24:39 +0900

    fix ex02,ex03 main

--- module02/ex03/main.cpp
+++ module02/ex03/main.cpp
@@ -2,12 +2,13 @@
 
 int main() {
 	Point a(0,0);
-	Point b(2,0);
+	Point b(3,3);
 	Point c(1,2);
 
 	std::cout << bsp(a, b, c, Point(1, 1)) << std::endl;
 	std::cout << bsp(a, b, c, Point(0.5, 0.5)) << std::endl;
-	std::cout << bsp(a, b, c, Point(3, 1)) << std::endl;
+	std::cout << bsp(a, b, c, Point(1, 1.5)) << std::endl;
+	std::cout << bsp(a, b, c, Point(3, 3)) << std::endl;
 	std::cout << bsp(a, b, c, Point(1, 3)) << std::endl;
 	std::cout << bsp(a, b, c, Point(2, 0)) << std::endl;
 }
