a29c9fb4ca2f8b9a6f5284777113c5fd0706424c
Gunjoong Kim <rjsoownd0419@gmail.com>
2023-11-14 12:43:22 +0900

    Add: Array.tpp

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -6,81 +6,20 @@ template <typename T>
 class Array
 {
 public:
-	Array()
-		: mData(NULL)
-		, mSize(0)
-	{}
+	Array();
+	Array(unsigned int n);
+	Array(const Array<T>& other);
+	~Array();
 
-	Array(unsigned int n)
-		: mData(new T[n])
-		, mSize(n)
-	{}
+	Array& operator=(const Array& other);
+	T& operator[](const unsigned int n);
+	const T& operator[](const unsigned int n) const;
 
-	Array(const Array<T>& other)
-	{
-		if (other.mData == NULL)
-		{
-			mData = NULL;
-			mSize = 0;
-			return ;
-		}
-
-		mData = new T[other.mSize];
-		for (unsigned int i = 0; i < other.mSize; i++)
-		{
-			mData[i] = other.mData[i];
-		}
-		mSize = other.mSize;
-	}
-
-	Array& operator=(const Array& other)
-	{
-		if (this == &other)
-			return *this;
-		if (mData != NULL)
-			delete[] mData;
-		if (other.mData == NULL)
-		{
-			mData = NULL;
-			mSize = 0;
-			return *this;
-		}
-
-		mData = new T[other.mSize];
-		for (unsigned int i = 0; i < other.mSize; i++)
-		{
-			mData[i] = other.mData[i];
-		}
-		mSize = other.mSize;
-		return *this;
-	}
-
-	T& operator[](const unsigned int n)
-	{
-		if (n >= mSize)
-			throw std::exception();
-		return mData[n];
-	}
-
-	const T& operator[](const unsigned int n) const
-	{
-		if (n >= mSize)
-			throw std::exception();
-		return mData[n];
-	}
-
-	unsigned int size() const
-	{
-		return mSize;
-	}
-
-	~Array()
-	{
-		if (mData != NULL)
-			delete[] mData;
-	}
+	unsigned int size() const;
 
 private:
 	T* mData;
 	unsigned int mSize;
 };
+
+#include "Array.tpp"
---
+++ module07/ex02/Array.tpp
@@ -0,0 +1,83 @@
+#include "Array.hpp"
+
+template <typename T>
+Array<T>::Array()
+	: mData(NULL)
+	, mSize(0)
+{}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+	: mData(new T[n])
+	, mSize(n)
+{}
+
+template <typename T>
+Array<T>::Array(const Array<T>& other)
+{
+	if (other.mData == NULL)
+	{
+		mData = NULL;
+		mSize = 0;
+		return ;
+	}
+
+	mData = new T[other.mSize];
+	for (unsigned int i = 0; i < other.mSize; i++)
+	{
+		mData[i] = other.mData[i];
+	}
+	mSize = other.mSize;
+}
+
+template <typename T>
+Array<T>& Array<T>::operator=(const Array<T>& other)
+{
+	if (this == &other)
+		return *this;
+	if (mData != NULL)
+		delete[] mData;
+	if (other.mData == NULL)
+	{
+		mData = NULL;
+		mSize = 0;
+		return *this;
+	}
+
+	mData = new T[other.mSize];
+	for (unsigned int i = 0; i < other.mSize; i++)
+	{
+		mData[i] = other.mData[i];
+	}
+	mSize = other.mSize;
+	return *this;
+}
+
+template <typename T>
+T& Array<T>::operator[](const unsigned int n)
+{
+	if (n >= mSize)
+		throw std::exception();
+	return mData[n];
+}
+
+template <typename T>
+const T& Array<T>::operator[](const unsigned int n) const
+	{
+		if (n >= mSize)
+			throw std::exception();
+		return mData[n];
+	}
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+	return mSize;
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	if (mData != NULL)
+		delete[] mData;
+}
