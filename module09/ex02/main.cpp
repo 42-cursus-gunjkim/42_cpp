1f0b46f4780d632fe60b7567dba96a03deb0a730
gunjoong-kim <rjsoownd0419@gmail.com>
2023-11-23 12:55:49 +0900

    Module09 ex02 init

---
+++ module09/ex02/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "PmergeMe.hpp"
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		std::cout << "Please hand over only one file name as a parameter for the program" << std::endl;
+		return 0;
+	}
+
+	return 0;
+}
