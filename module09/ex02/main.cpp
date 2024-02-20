572cfe4f53cc6a88a37ca98cb19a03ff97fcc7a8
gunjoong-kim <rjsoownd0419@gmail.com>
2024-02-20 22:37:28 +0900

    ex03 main

--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include "PmergeMe.hpp"
 
-int main(int argc, char* argv[])
+bool CheckInput(int argc, char** argv, std::vector<int>* outInput)
 {
-	if (argc != 2)
+	if (argc < 2)
 	{
-		std::cout << "Please hand over only one file name as a parameter for the program" << std::endl;
-		return 0;
+		std::cout << "Please hand over a positive integer sequence as argument!" << std::endl;
+		return false;
 	}
 
+	for (int i = 1; i < argc; i++)
+	{
+		int num = std::atoi(argv[i]);
+		if (num <= 0)
+		{
+			std::cout << "Please hand over a positive integer sequence as argument!" << std::endl;
+			return false;
+		}	
+		outInput->push_back(num);
+	}
+
+	return true;
+}
+
+int main(int argc, char* argv[])
+{
+	std::vector<int> input;
+	if (CheckInput(argc, argv, &input) == false)
+		return 1;
+
+	// print input
+	for (int i = 0; i < (int)input.size(); i++)
+	{
+		std::cout << input[i] << " ";
+	}
+	std::cout << std::endl;
+	
 	return 0;
 }
